deaf47d84a1929bab17bfed25aa7a00e3c983c96
Achinths Isuru <achinthaisuru444@gmail.com>
2020-10-08 20:52:53 +0530

    small formatting change

--- lib/presentation/widgets/diary_form.dart
+++ lib/presentation/widgets/diary_form.dart
@@ -24,83 +24,85 @@ class _DiaryFormState extends State<DiaryForm> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: [
-      Form(
-        key: _formKey,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            AnimatedContainer(
-              duration: Duration(milliseconds: 300),
-              width: _isExpanded
-                  ? MediaQuery.of(context).size.width
-                  : MediaQuery.of(context).size.width / 2,
-              child: DiaryTextField(
-                maxLines: 1,
-                labelText: 'Submit New',
-                color: Color(0xff2F9DDC),
-                context: context,
-                errorText: 'Missing title',
-                onTap: () {
-                  setState(() {
-                    this._isExpanded = true;
-                  });
-                },
-                onChange: (value) {
-                  this._title = value;
-                },
-                controller: this._controllerTitle,
-              ),
-            ),
-            AnimatedContainer(
-              duration: Duration(milliseconds: 300),
-              curve: Curves.easeIn,
-              height:
-                  _isExpanded ? MediaQuery.of(context).size.height / 3.15 : 0,
-              child: SingleChildScrollView(
-                child: Column(
-                  children: [
-                    Padding(padding: EdgeInsets.only(top: 5, bottom: 5)),
-                    DiaryTextField(
-                      maxLines: 6,
-                      labelText: 'Enter Description',
-                      color: Color(0xff15ABDA),
-                      context: context,
-                      errorText: 'Missing Description',
-                      onChange: (value) {
-                        this._desc = value;
-                      },
-                      controller: this._controllerDesc,
-                    ),
-                    Padding(padding: EdgeInsets.only(top: 5)),
-                    Container(
-                      width: MediaQuery.of(context).size.width,
-                      child: RaisedButton(
-                        child: Text(
-                          'SUBMIT',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.w500,
-                              letterSpacing: 1.5),
-                        ),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: new BorderRadius.circular(15),
-                        ),
-                        color: Color(0xff1E76F0).withOpacity(0.7),
-                        elevation: 0,
-                        onPressed: _submitFunction,
-                      ),
-                    )
-                  ],
+    return Column(
+      children: [
+        Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              AnimatedContainer(
+                duration: Duration(milliseconds: 300),
+                width: _isExpanded
+                    ? MediaQuery.of(context).size.width
+                    : MediaQuery.of(context).size.width / 2,
+                child: DiaryTextField(
+                  maxLines: 1,
+                  labelText: 'Submit New',
+                  color: Color(0xff2F9DDC),
+                  context: context,
+                  errorText: 'Missing title',
+                  onTap: () {
+                    setState(() {
+                      this._isExpanded = true;
+                    });
+                  },
+                  onChange: (value) {
+                    this._title = value;
+                  },
+                  controller: this._controllerTitle,
                 ),
               ),
-            ),
-          ],
+              AnimatedContainer(
+                duration: Duration(milliseconds: 300),
+                curve: Curves.easeIn,
+                height:
+                    _isExpanded ? MediaQuery.of(context).size.height / 3.15 : 0,
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      Padding(padding: EdgeInsets.only(top: 5, bottom: 5)),
+                      DiaryTextField(
+                        maxLines: 6,
+                        labelText: 'Enter Description',
+                        color: Color(0xff15ABDA),
+                        context: context,
+                        errorText: 'Missing Description',
+                        onChange: (value) {
+                          this._desc = value;
+                        },
+                        controller: this._controllerDesc,
+                      ),
+                      Padding(padding: EdgeInsets.only(top: 5)),
+                      Container(
+                        width: MediaQuery.of(context).size.width,
+                        child: RaisedButton(
+                          child: Text(
+                            'SUBMIT',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.w500,
+                                letterSpacing: 1.5),
+                          ),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: new BorderRadius.circular(15),
+                          ),
+                          color: Color(0xff1E76F0).withOpacity(0.7),
+                          elevation: 0,
+                          onPressed: _submitFunction,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
-      ),
-      Padding(padding: EdgeInsets.only(top: 7.5, bottom: 7.5)),
-    ]);
+        Padding(padding: EdgeInsets.only(top: 7.5, bottom: 7.5)),
+      ],
+    );
   }
 
   void _submitFunction() {
@@ -112,7 +114,8 @@ class _DiaryFormState extends State<DiaryForm> {
       );
       this._controllerTitle.clear();
       this._controllerDesc.clear();
-      BlocProvider.of<DiaryBloc>(context).add(AddDiaryCard(this._title,this._desc));
+      BlocProvider.of<DiaryBloc>(context)
+          .add(AddDiaryCard(this._title, this._desc));
       //.dispatch(AddDiaryCard(user: user));
     }
   }
